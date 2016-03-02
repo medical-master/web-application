458fa7bc7635aa099eb088e07a6a010b307929a8
He, Jiehui <he_jiehui@163.com>
2016-03-02 18:15:34 +0800

    w

--- web/src/main/java/com/medicalmaster/web/control/user/ActivateMasterAction.java
+++ web/src/main/java/com/medicalmaster/web/control/user/ActivateMasterAction.java
@@ -1,5 +1,34 @@
 package com.medicalmaster.web.control.user;
 
-public class ActivateMasterAction {
+import com.medicalmaster.common.user.CreateUserRequest;
+import com.medicalmaster.web.control.WebContext;
+import com.xross.tools.xunit.Context;
+import com.xross.tools.xunit.Processor;
 
+public class ActivateMasterAction implements Processor{
+
+	@Override
+	public void process(Context ctx) {
+		WebContext context = (WebContext)ctx;
+	}
+	
+	private CreateUserRequest normalize(WebContext context) {
+		CreateUserRequest req = new CreateUserRequest();
+		return req;
+	}
+	
+	private <T> T normalize(WebContext context, Class<T> clazz) {
+		T value =null;
+		try {
+			value = clazz.newInstance();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return value;
+	}
+	
 }
