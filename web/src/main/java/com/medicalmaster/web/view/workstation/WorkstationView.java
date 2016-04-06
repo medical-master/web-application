ee2fc19abb8af7891e34333c9d7e97f05d4e7fcd
jiangsong-java <js-java@163.com>
2016-04-06 15:53:56 +0800

    工作站view修改

--- web/src/main/java/com/medicalmaster/web/view/workstation/WorkstationView.java
+++ web/src/main/java/com/medicalmaster/web/view/workstation/WorkstationView.java
@@ -3,13 +3,28 @@ package com.medicalmaster.web.view.workstation;
 import java.util.List;
 
 import com.medicalmaster.dal.EdsWorkstation;
+import com.xross.tools.xunit.Context;
+import com.xross.tools.xunit.Processor;
 
-public class WorkstationView {
-	public List<EdsWorkstation> displayWorkstations() {
+public class WorkstationView
+{
+	public List<EdsWorkstation> displayWorkstations() 
+	{
+		System.out.println("dddddddddddddddd");
 		return null;
 	}
 	
-	public EdsWorkstation getWorkstation(int workStationId) {
+	public EdsWorkstation getWorkstation(int workStationId) 
+	{
+		System.out.println("ddddddddddddddddffffffffffff");
 		return null;
 	}
 }
+
+/**
+ * Revision History
+ * -------------------------------------------------------------------------
+ * Version       Date             Author          		Note
+ * -------------------------------------------------------------------------
+ * 1.0         2016-04-01        	js        	  	    工作站信息显示
+ */
