81f28163563b0edf201254b9269f726a53437acd
He, Jiehui <he_jiehui@163.com>
2016-04-12 11:16:46 +0800

    add missing dao pojo

--- common/src/main/java/com/medicalmaster/dal/WorkstationViewDao.java
+++ common/src/main/java/com/medicalmaster/dal/WorkstationViewDao.java
@@ -20,16 +20,20 @@ public class WorkstationViewDao {
 
 	private DalRowMapper<ResearchStageRecordPojo> researchStageRecordPojoRowMapper = null;
 
+	private DalRowMapper<WorkstationViewPojoPojo> workstationViewPojoPojoRowMapper = null;
+
+
 	public WorkstationViewDao() throws SQLException {
 		this.trainMaterialRecorderPojoRowMapper = new DalDefaultJpaMapper(TrainMaterialRecorderPojo.class);
 		this.researchStageRecordPojoRowMapper = new DalDefaultJpaMapper(ResearchStageRecordPojo.class);
+		this.workstationViewPojoPojoRowMapper = new DalDefaultJpaMapper(WorkstationViewPojoPojo.class);
 		this.queryDao = new DalQueryDao(DATA_BASE);
 	}
 	/**
-	 * a
+	 * findTrainMaterial
 	**/
 	public List<TrainMaterialRecorderPojo> findTrainMaterial(Integer materialId, DalHints hints) throws SQLException {
-		String sql = "SELECT a.title, a.description, b.resourceName, b.resourceType, b.fileUrl, b.allowDownload, b.downloadCnt FROM train_material a LEFT JOIN sys_resource b ON a.materialFile = b.id AND a.id = ? WHERE b.`status` = 1;";
+		String sql = "SELECT a.title, a.description, b.resourceName, b.resourceType, b.fileUrl, b.allowDownload, b.downloadCnt FROM train_material a LEFT JOIN sys_resource b ON a.materialFile = b.id AND a.id = ? WHERE b.`status` = 1";
 		StatementParameters parameters = new StatementParameters();
 		hints = DalHints.createIfAbsent(hints);
 		int i = 1;
@@ -37,17 +41,37 @@ public class WorkstationViewDao {
 		return (List<TrainMaterialRecorderPojo>)queryDao.query(sql, parameters, hints, trainMaterialRecorderPojoRowMapper);
 	}
 	/**
-	 * a
+	 * findResearchStage
 	**/
 	public List<ResearchStageRecordPojo> findResearchStage(Integer workstationId, int pageNo, int pageSize, DalHints hints) throws SQLException {
 		String sql = "SELECT stage.clinicalResearchId, stage.title AS stageTitle, stage.description AS stage, stage.resourceId AS stageResourceId, stage.viewStatus AS stageViewStatus, stage.`status` AS stageStatus, stage.publishTime AS stagePublishTime, stage.createTime AS stageCreateTime, stage.createUser AS stageCreateUser, stage.lastUpdateTime AS stageLastUpdateTime, stage.lastUpdateUser AS stageLastUpdateUser, research.title, research.description, research.resourceId, research.viewStatus, research.currentStatus, research.publishTime, research.createTime, research.createUser, research.lastUpdateTime, research.lastUpdateUser FROM clinical_research_stage stage, clinical_research research WHERE stage.clinicalResearchId = research.id AND research.workstationId = ? limit ?, ?";
 		StatementParameters parameters = new StatementParameters();
 		hints = DalHints.createIfAbsent(hints);
 		int i = 1;
-		parameters.set(i++, "workstationId", Types.INTEGER, workstationId);
+		parameters.setSensitive(i++, "workstationId", Types.INTEGER, workstationId);
 		parameters.set(i++, Types.INTEGER, (pageNo - 1) * pageSize);
 		parameters.set(i++, Types.INTEGER, pageSize);
 		return (List<ResearchStageRecordPojo>)queryDao.query(sql, parameters, hints, researchStageRecordPojoRowMapper);
+	}
+	/**
+	 * 工作站信息查询
+	**/
+	public List<WorkstationViewPojoPojo> showWorkstation(Integer workstationId, DalHints hints) throws SQLException {
+		String sql = "SELECT wks.`name` as wksName, wks.description, wks.domains, wks.illCode, wks.members, wks.attends, wks.status as wksStatus, ex.expertArea, ex.honorHtml, ex.academicHtml, us.name as usName, us.nickName, us.sex, us.email, concat(us.professionalRank,"，",us.title,"，",us.educationLevel) as pte, us.status usStatus, us.doctorNumber, us.department, ho.name as hospitalName, ho.level, ho.proviceId, ho.cityId, ho.districtId, ho.address, re.fileUrl FROM workstation wks, expert ex, USER us, sys_hospital ho, sys_resource re where wks.expertId=ex.id and ex.userId=us.userId and us.hosptialId=ho.hospitalId and us.iconResourceId=re.id and wks.status=1 and wks.workstationId=?";
+		StatementParameters parameters = new StatementParameters();
+		hints = DalHints.createIfAbsent(hints);
+		int i = 1;
+		parameters.setSensitive(i++, "workstationId", Types.INTEGER, workstationId);
+		return (List<WorkstationViewPojoPojo>)queryDao.query(sql, parameters, hints, workstationViewPojoPojoRowMapper);
+	}
+	/**
+	 * 获取总页数
+	**/
+	public WorkstationViewPojoPojo count(DalHints hints) throws SQLException {
+		String sql = "SELECT wks.`name` as wksName, wks.description, wks.domains, wks.illCode, wks.members, wks.attends, wks.status as wksStatus, ex.expertArea, ex.honorHtml, ex.academicHtml, us.name as usName, us.nickName, us.sex, us.email, concat(us.professionalRank,"，",us.title,"，",us.educationLevel) as pte, us.status usStatus, us.doctorNumber, us.department, ho.name as hospitalName, ho.level, ho.proviceId, ho.cityId, ho.districtId, ho.address, re.fileUrl FROM workstation wks, expert ex, USER us, sys_hospital ho, sys_resource re where wks.expertId=ex.id and ex.userId=us.userId and us.hosptialId=ho.hospitalId and us.iconResourceId=re.id and wks.status=1";
+		StatementParameters parameters = new StatementParameters();
+		hints = DalHints.createIfAbsent(hints);
+		return (WorkstationViewPojoPojo)queryDao.queryForObjectNullable(sql, parameters, hints, workstationViewPojoPojoRowMapper);
 	}
 
 }
---
+++ common/src/main/java/com/medicalmaster/dal/WorkstationViewPojoPojo.java
@@ -0,0 +1,321 @@
+package com.medicalmaster.dal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import com.ctrip.platform.dal.dao.annotation.Database;
+import com.ctrip.platform.dal.dao.annotation.Sensitive;
+import com.ctrip.platform.dal.dao.annotation.Type;
+import java.sql.Types;
+
+import com.ctrip.platform.dal.dao.DalPojo;
+
+@Entity
+@Database(name="")
+@Table(name="")
+public class WorkstationViewPojoPojo implements DalPojo {
+	
+	@Column(name="wksName")
+	@Type(value=Types.VARCHAR)
+	private String wksName;
+	
+	@Column(name="description")
+	@Type(value=Types.VARCHAR)
+	private String description;
+	
+	@Column(name="domains")
+	@Type(value=Types.VARCHAR)
+	private String domains;
+	
+	@Column(name="illCode")
+	@Type(value=Types.INTEGER)
+	private Integer illCode;
+	
+	@Column(name="members")
+	@Type(value=Types.INTEGER)
+	private Integer members;
+	
+	@Column(name="attends")
+	@Type(value=Types.INTEGER)
+	private Integer attends;
+	
+	@Column(name="wksStatus")
+	@Type(value=Types.INTEGER)
+	private Integer wksStatus;
+	
+	@Column(name="expertArea")
+	@Type(value=Types.VARCHAR)
+	private String expertArea;
+	
+	@Column(name="honorHtml")
+	@Type(value=Types.INTEGER)
+	private Integer honorHtml;
+	
+	@Column(name="academicHtml")
+	@Type(value=Types.INTEGER)
+	private Integer academicHtml;
+	
+	@Column(name="usName")
+	@Type(value=Types.VARCHAR)
+	private String usName;
+	
+	@Column(name="nickName")
+	@Type(value=Types.VARCHAR)
+	private String nickName;
+	
+	@Column(name="sex")
+	@Type(value=Types.BIT)
+	private Boolean sex;
+	
+	@Column(name="email")
+	@Type(value=Types.VARCHAR)
+	private String email;
+	
+	@Column(name="pte")
+	@Type(value=Types.VARCHAR)
+	private String pte;
+	
+	@Column(name="usStatus")
+	@Type(value=Types.INTEGER)
+	private Integer usStatus;
+	
+	@Column(name="doctorNumber")
+	@Type(value=Types.VARCHAR)
+	private String doctorNumber;
+	
+	@Column(name="department")
+	@Type(value=Types.VARCHAR)
+	private String department;
+	
+	@Column(name="hospitalName")
+	@Type(value=Types.VARCHAR)
+	private String hospitalName;
+	
+	@Column(name="level")
+	@Type(value=Types.INTEGER)
+	private Integer level;
+	
+	@Column(name="proviceId")
+	@Type(value=Types.INTEGER)
+	private Integer proviceId;
+	
+	@Column(name="cityId")
+	@Type(value=Types.INTEGER)
+	private Integer cityId;
+	
+	@Column(name="districtId")
+	@Type(value=Types.INTEGER)
+	private Integer districtId;
+	
+	@Column(name="address")
+	@Type(value=Types.VARCHAR)
+	private String address;
+	
+	@Column(name="fileUrl")
+	@Type(value=Types.VARCHAR)
+	private String fileUrl;
+
+	public String getWksName() {
+		return wksName;
+	}
+
+	public void setWksName(String wksName) {
+		this.wksName = wksName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDomains() {
+		return domains;
+	}
+
+	public void setDomains(String domains) {
+		this.domains = domains;
+	}
+
+	public Integer getIllCode() {
+		return illCode;
+	}
+
+	public void setIllCode(Integer illCode) {
+		this.illCode = illCode;
+	}
+
+	public Integer getMembers() {
+		return members;
+	}
+
+	public void setMembers(Integer members) {
+		this.members = members;
+	}
+
+	public Integer getAttends() {
+		return attends;
+	}
+
+	public void setAttends(Integer attends) {
+		this.attends = attends;
+	}
+
+	public Integer getWksStatus() {
+		return wksStatus;
+	}
+
+	public void setWksStatus(Integer wksStatus) {
+		this.wksStatus = wksStatus;
+	}
+
+	public String getExpertArea() {
+		return expertArea;
+	}
+
+	public void setExpertArea(String expertArea) {
+		this.expertArea = expertArea;
+	}
+
+	public Integer getHonorHtml() {
+		return honorHtml;
+	}
+
+	public void setHonorHtml(Integer honorHtml) {
+		this.honorHtml = honorHtml;
+	}
+
+	public Integer getAcademicHtml() {
+		return academicHtml;
+	}
+
+	public void setAcademicHtml(Integer academicHtml) {
+		this.academicHtml = academicHtml;
+	}
+
+	public String getUsName() {
+		return usName;
+	}
+
+	public void setUsName(String usName) {
+		this.usName = usName;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public Boolean getSex() {
+		return sex;
+	}
+
+	public void setSex(Boolean sex) {
+		this.sex = sex;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPte() {
+		return pte;
+	}
+
+	public void setPte(String pte) {
+		this.pte = pte;
+	}
+
+	public Integer getUsStatus() {
+		return usStatus;
+	}
+
+	public void setUsStatus(Integer usStatus) {
+		this.usStatus = usStatus;
+	}
+
+	public String getDoctorNumber() {
+		return doctorNumber;
+	}
+
+	public void setDoctorNumber(String doctorNumber) {
+		this.doctorNumber = doctorNumber;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getHospitalName() {
+		return hospitalName;
+	}
+
+	public void setHospitalName(String hospitalName) {
+		this.hospitalName = hospitalName;
+	}
+
+	public Integer getLevel() {
+		return level;
+	}
+
+	public void setLevel(Integer level) {
+		this.level = level;
+	}
+
+	public Integer getProviceId() {
+		return proviceId;
+	}
+
+	public void setProviceId(Integer proviceId) {
+		this.proviceId = proviceId;
+	}
+
+	public Integer getCityId() {
+		return cityId;
+	}
+
+	public void setCityId(Integer cityId) {
+		this.cityId = cityId;
+	}
+
+	public Integer getDistrictId() {
+		return districtId;
+	}
+
+	public void setDistrictId(Integer districtId) {
+		this.districtId = districtId;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getFileUrl() {
+		return fileUrl;
+	}
+
+	public void setFileUrl(String fileUrl) {
+		this.fileUrl = fileUrl;
+	}
+
+}
