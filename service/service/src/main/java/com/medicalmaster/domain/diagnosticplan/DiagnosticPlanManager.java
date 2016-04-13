3296b555e7e485de52645c5729c22eeac51cb95e
jiangsong-java <js-java@163.com>
2016-04-13 10:47:30 +0800

    工作站

--- service/service/src/main/java/com/medicalmaster/domain/diagnosticplan/DiagnosticPlanManager.java
+++ service/service/src/main/java/com/medicalmaster/domain/diagnosticplan/DiagnosticPlanManager.java
@@ -7,70 +7,72 @@ import com.medicalmaster.common.diagnosticplan.QueryDiagPlansRequest;
 import com.medicalmaster.dal.DiagnosticPlan;
 import com.medicalmaster.dal.DiagnosticPlanDao;
 
-public class DiagnosticPlanManager {
+public class DiagnosticPlanManager 
+{
 	private DiagnosticPlanDao dao;
 
 	/**
 	 * 创建构造方法
-	 * 
 	 * @throws SQLException
 	 */
-	public DiagnosticPlanManager() throws SQLException {
+	public DiagnosticPlanManager() throws SQLException 
+	{
 		dao = new DiagnosticPlanDao();
 	}
 
 	/**
 	 * 获取整个工作空间数据
-	 * 
 	 * @throws SQLException
 	 * @return List<EdsWorkstation>
 	 */
-	public List<DiagnosticPlan> getAllDiagnosticPlan() throws SQLException {
+	public List<DiagnosticPlan> getAllDiagnosticPlan() throws SQLException 
+	{
 		return dao.getAll(null);
 	}
 
 	/**
 	 * 修改工作站信息
-	 * 
 	 * @throws SQLException
 	 */
-	public void updateDiagnosticPlan(DiagnosticPlan diagnosticPlan) throws SQLException {
+	public void updateDiagnosticPlan(DiagnosticPlan diagnosticPlan) throws SQLException 
+	{
 		dao.update(null, diagnosticPlan);
 	}
 
 	/**
 	 * 删除工作站信息
-	 * 
 	 * @throws SQLException
 	 */
-	public int removeDiagnosticPlan(Integer diagnosticPlanId) throws SQLException {
+	public int removeDiagnosticPlan(Integer diagnosticPlanId) throws SQLException 
+	{
 		DiagnosticPlan diagnosticPlan = new DiagnosticPlan();
 		diagnosticPlan.setId(diagnosticPlanId);
 		return dao.delete(null, diagnosticPlan);
 	}
 
 	/**
-	 * 添加工作站信息
-	 * 
+	 * 添加工作站信息 
 	 * @throws SQLException
 	 */
-	public void insertWorkstationFollower() throws SQLException {
+	public void insertWorkstationFollower() throws SQLException 
+	{
 		DiagnosticPlan diagnosticPlan = new DiagnosticPlan();
 		dao.insert(null, diagnosticPlan);
 	}
 
 	/**
-	 * 查询信息
-	 * 
+	 * 查询信息 
 	 * @param id
 	 * @return
 	 * @throws SQLException
 	 */
-	public DiagnosticPlan queryInfo(Integer id) throws SQLException {
+	public DiagnosticPlan queryInfo(Integer id) throws SQLException 
+	{
 		return dao.queryByPk(id, null);
 	}
 
-	public Integer findDiagnosticPlansCnt(QueryDiagPlansRequest request) throws SQLException {
+	public Integer findDiagnosticPlansCnt(QueryDiagPlansRequest request) throws SQLException 
+	{
 		List<DiagnosticPlan> plans = dao.findAllDiagnosticPlans(request.getWorkstationId(), request.getPublishStatus(),
 				null);
 		if (plans == null) {
@@ -79,8 +81,15 @@ public class DiagnosticPlanManager {
 
 		return plans.size();
 	}
-
-	public List<DiagnosticPlan> findDiagnosticPlans(QueryDiagPlansRequest request) throws SQLException {
+	
+	/**
+	 * 获取工作站对应的相关诊疗方案信息
+	 * @param request
+	 * @return
+	 * @throws SQLException
+	 */
+	public List<DiagnosticPlan> findDiagnosticPlans(QueryDiagPlansRequest request) throws SQLException 
+	{
 		return dao.findDiagnosticPlans(request.getWorkstationId(), request.getPublishStatus(), request.getPageNo(),
 				request.getPageSize(), null);
 	}
@@ -89,7 +98,7 @@ public class DiagnosticPlanManager {
 /**
  * Revision History
  * -------------------------------------------------------------------------
- * Version Date Author Note
- * ------------------------------------------------------------------------- 1.0
- * 2016-04-01 js 构造诊疗方案操类
+ * Version 				Date 				Author					Note
+ * ------------------------------------------------------------------------- 
+ * 1.0				2016-04-01 				 js 				构造诊疗方案操类
  */
--- service/service/src/main/java/com/medicalmaster/domain/workstation/WorkstationManager.java
+++ service/service/src/main/java/com/medicalmaster/domain/workstation/WorkstationManager.java
@@ -19,10 +19,10 @@ public class WorkstationManager {
 
 	/**
 	 * 创建构造方法
-	 * 
 	 * @throws SQLException
 	 */
-	public WorkstationManager() throws SQLException {
+	public WorkstationManager() throws SQLException 
+	{
 		viewDao = new WorkstationViewDao();
 		dao = new WorkstationDao();
 		extDao = new WorkstationExtDao();
@@ -30,29 +30,29 @@ public class WorkstationManager {
 
 	/**
 	 * 获取整个工作空间数据
-	 * 
 	 * @throws SQLException
 	 * @return List<EdsWorkstation>
 	 */
-	public List<WorkstationViewPojoPojo> getAllWorkstation(QueryWorkstationRequest request) throws SQLException {
+	public List<WorkstationViewPojoPojo> getAllWorkstation(QueryWorkstationRequest request) throws SQLException 
+	{
 		return viewDao.showWorkstation(request.getPageNo(), request.getPageSize(), null);
 	}
 
 	/**
 	 * 修改工作站信息
-	 * 
 	 * @throws SQLException
 	 */
-	public void updateWorkstation(Workstation edsWorkstation) throws SQLException {
+	public void updateWorkstation(Workstation edsWorkstation) throws SQLException 
+	{
 		dao.update(null, edsWorkstation);
 	}
 
 	/**
 	 * 删除工作站信息
-	 * 
 	 * @throws SQLException
 	 */
-	public int removeWorkstation(Integer workstationId) throws SQLException {
+	public int removeWorkstation(Integer workstationId) throws SQLException 
+	{
 		Workstation workstation = new Workstation();
 		workstation.setWorkstationId(workstationId);
 		return dao.delete(null, workstation);
@@ -60,17 +60,16 @@ public class WorkstationManager {
 
 	/**
 	 * 添加工作站信息
-	 * 
 	 * @throws SQLException
 	 */
-	public void insertWorkstation() throws SQLException {
+	public void insertWorkstation() throws SQLException 
+	{
 		Workstation workstation = new Workstation();
 		dao.insert(null, workstation);
 	}
 
 	/**
 	 * 查询指定的工作站详细信息
-	 * 
 	 * @param id
 	 * @return
 	 * @throws SQLException
@@ -80,18 +79,36 @@ public class WorkstationManager {
 	}
 
 	/**
-	 * 获取总条数
+	 * 获取首页工作站总数
+	 * @param request
+	 * @return Integer
+	 * @throws SqlException
 	 */
-	public Integer countWorkstation(QueryWorkstationRequest request) throws SQLException {
+	public Integer countWorkstation(QueryWorkstationRequest request) throws SQLException 
+	{
 		Long count = viewDao.count(null);
 		return Integer.parseInt(count.toString());
 	}
-
-	public Integer queryWorksationListCnt(QueryWorkstationListRequest request) throws SQLException {
+	
+	/**
+	 * 获取首页工作站列表总数
+	 * @param request
+	 * @return Integer
+	 * @throws SqlException
+	 */
+	public Integer queryWorksationListCnt(QueryWorkstationListRequest request) throws SQLException 
+	{
 		return extDao.findWorkstationsCnt(request.getStatus(), null).intValue();
 	}
 
-	public List<Workstation> queryWorkstationList(QueryWorkstationListRequest request) throws SQLException {
+	/**
+	 * 获取首页工作站列表数据
+	 * @param request
+	 * @return
+	 * @throws SQLException
+	 */
+	public List<Workstation> queryWorkstationList(QueryWorkstationListRequest request) throws SQLException 
+	{
 		return dao.findWorkstations(request.getStatus(), request.getPageNo(), request.getPageSize(), null);
 	}
 }
@@ -99,7 +116,7 @@ public class WorkstationManager {
 /**
  * Revision History
  * -------------------------------------------------------------------------
- * Version Date Author Note
- * ------------------------------------------------------------------------- 1.0
- * 2016-04-01 js 构造工作站基本信息操类
+ * Version 				Date 				Author					Note
+ * ------------------------------------------------------------------------- 
+ * 1.0				2016-04-01 				 js 			构造工作站基本信息操类
  */
