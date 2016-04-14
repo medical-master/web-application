5ea587538ba6b33d792ad3961656fbd281281421
jiangsong-java <js-java@163.com>
2016-04-14 17:09:10 +0800

    工作站

--- service/service/src/main/java/com/medicalmaster/resource/TrainMaterialResource.java
+++ service/service/src/main/java/com/medicalmaster/resource/TrainMaterialResource.java
@@ -1,9 +1,15 @@
 package com.medicalmaster.resource;
 
 import javax.annotation.Resource;
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import com.medicalmaster.common.bean.ResourceConstants;
+import com.medicalmaster.common.trainmaterial.QueryTrainMaterialRequest;
+import com.medicalmaster.common.trainmaterial.QueryTrainMaterialResponse;
 
 /**
  * 培训资料
@@ -16,8 +22,27 @@ import com.medicalmaster.common.bean.ResourceConstants;
  */
 @Resource
 @Path(ResourceConstants.PATH_TRAIN_MATERIAL)
-public class TrainMaterialResource extends Resources {
-	public TrainMaterialResource() {
-		super(null, null);
+public class TrainMaterialResource extends Resources 
+{
+	public TrainMaterialResource() 
+	{
+		super("trainMaterial.xunit", "trainMaterial branch");
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/showTrainMaterial")
+	public QueryTrainMaterialResponse showWorkTrainMaterial(@BeanParam QueryTrainMaterialRequest request) {
+		request.setAction("showInfo");
+		return handle(request, request.getAction());
 	}
 }
+
+/**
+ * Revision History
+ * -------------------------------------------------------------------------
+ * Version       Date             Author          		Note
+ * -------------------------------------------------------------------------
+ * 1.0         2016-04-14        	js        	  	    培训资料公共类
+ */
+
